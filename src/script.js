41f5849d41053187d82a046779868b24b123d76a
ptrckmgl <patricksantiaguel@gmail.com>
2025-03-31 13:11:14 +0800

    Added upgrade option with exponential growth

--- src/script.js
+++ src/script.js
@@ -1,21 +1,43 @@
 import './style.css'
 
 let count = 0;
+let clickValue = 1;
+let basePrice = 10;
+let currentPrice = basePrice;
 
 const crinklesImg = document.getElementById("crnikles")
 const countDislpay = document.getElementById("counter")
 const resetBtn = document.getElementById("reset")
+const upgrade1Btn = document.getElementById("upgrade1")
+const priceText = document.getElementById("price")
 
 crinkles.addEventListener("click", () => {
-    count++;
+    count += clickValue;
     updateCounter();
 })
 
 resetBtn.addEventListener("click", () => {
     count = 0;
+    clickValue = 1;
     updateCounter();
 })
 
 function updateCounter() {
     countDislpay.textContent = count;
+}
+
+upgrade1Btn.addEventListener("click", () => {
+    if (count >= currentPrice) {
+        clickValue += 1.5;
+        count -= currentPrice;
+        updatePrice();
+        updateCounter();
+    } else {
+        console.log(currentPrice)
+    }
+})
+
+function updatePrice() {
+    currentPrice = basePrice += 1.5; 
+    priceText.textContent = currentPrice;
 }
